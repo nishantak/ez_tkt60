f150c524f5d6f53783781334dcac3a21ffdfc065
Nishantak Panigrahi <65376050+nishantak@users.noreply.github.com>
2024-02-08 16:09:06 +0530

    .

--- get_60_tickets.py
+++ get_60_tickets.py
@@ -8,6 +8,8 @@ from selenium.webdriver.common.keys import Keys
 try:
     options = Options()
     options.headless = True
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
     driver = webdriver.Chrome(options=options)
 
     # Open BKMS
