83fce89473e51ef9353f71f169c8a3f159008270
nishantak09 <nishantak09@gmail.com>
2024-02-08 20:56:11 +0530

    enwgnw

--- get_60_tickets.py
+++ get_60_tickets.py
@@ -4,11 +4,12 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 
 try:
-    options = uc.ChromeOptions()
-    options.headless = True
-    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-
-    driver = uc.Chrome(options=options)
+    # options = uc.ChromeOptions()
+    # options.headless = True
+    # options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+    # options.add_argument("--disable-extensions")
+ 
+    driver = uc.Chrome()
 
     driver.delete_all_cookies()
 
@@ -24,19 +25,25 @@ try:
     driver.find_element(By.CLASS_NAME, "bwc__sc-ttnkwg-14.flGQbT").click()
     time.sleep(random.uniform(0.75, 1.25))
 
-    # Find search | Selects by XML path (better)
+    # Find search box | Selects by XML path (better)
     search_box = driver.find_element(By.XPATH, "//*[@id=\"4\"]")
     search_box.click()
     time.sleep(random.uniform(0.5,0.75))
-    input_field = driver.find_element(By.XPATH, "//*[@id=\"super-container\"]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div/div/div/input")
+
     # Search film
+    input_field = driver.find_element(By.XPATH, "//*[@id=\"super-container\"]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div/div/div/input")
     filmname = "Teri Baaton Mein Aisa Uljha Jiya" 
     input_field.send_keys(filmname)
+    time.sleep(random.uniform(0.5, 1))
+    driver.find_element(By.CLASS_NAME, "bwc__sc-1iyhybo-13.kqrJYR").click()
+    time.sleep(random.uniform(0.5, 1))
+    driver.find_element(By.XPATH, "//button[contains(., 'Book tickets')]").click()  # Selects dynamically (even better)
     time.sleep(1000)
 
+
 except Exception as e:
-    #print(e)
-    traceback.print_exc()
+    print(e)
+    # traceback.print_exc()
 
 finally:
     driver.delete_all_cookies()
