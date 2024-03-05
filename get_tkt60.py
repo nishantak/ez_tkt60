9c26aebf3afcaa9352b07f85fd1dc15ca2fa4374
nishantak09 <nishantak09@gmail.com>
2024-03-05 22:09:13 +0530

    theatre select

--- get_tkt60.py
+++ get_tkt60.py
@@ -1,6 +1,5 @@
 import time, traceback, random
 
-import undetected_chromedriver as uc
 from seleniumbase import Driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as ec
 
 # Increase sleep time if slow internet, or learn to implement expected_conditions
 try:
-    # options = uc.ChromeOptions()
+    # options = Driver.options.ChromeOptions()
     # options.headless = True
     # options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
     # options.add_argument("--disable-extensions")
@@ -37,13 +36,21 @@ try:
 
     # Search film
     input_field = driver.find_element(By.XPATH, "//*[@id=\"super-container\"]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div/div/div/input")
-    filmname = "Teri Baaton Mein Aisa Uljha Jiya" 
+    filmname = "Dune: Part Two" 
     input_field.send_keys(filmname)
     time.sleep(random.uniform(0.9, 1.3))
     driver.find_element(By.CLASS_NAME, "bwc__sc-1iyhybo-13.kqrJYR").click()
     time.sleep(random.uniform(0.5, 1))
     driver.find_element(By.XPATH, "//button[contains(., 'Book tickets')]").click()  # Selects dynamically (even better)
-    time.sleep(2)
+    time.sleep(random.uniform(0.5, 1))
+
+    # Booking specs
+    date = "06"
+    driver.find_element(By.XPATH, f"//div[contains(text(), {date})]").click()
+    time.sleep(random.uniform(0.5, 1))
+    driver.find_element(By.XPATH, "//a[@data-venue-code='IOSE' and @data-showtime-code='1200']").click()
+    
+    time.sleep(5)
 
 
 except Exception as e:
